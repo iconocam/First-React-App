29336b7c8ce45934fa3fa4476266c735f7073b9c
iconocam <150183122+iconocam@users.noreply.github.com>
2024-01-29 23:02:39 -0500

    I MADE A REACT APP WEEEWOOOWEEEWOOO

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from "./components/Footer"
+
+
+function Header() {
+  return (<h1>Simple React Application</h1>);
+}
+
+function Content(props) {
+  return (<p style= {{color: props.color}}>{props.text}</p>);
+}
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+    <Content color="blue" text="Sniper get down!!"/>
+    <Footer />
+    
+    
+    
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,3 @@
+export default function Footer() {
+    return <h1>Think again pestilent human for I am invincible! <b>*explodes*</b> </h1>;
+}
